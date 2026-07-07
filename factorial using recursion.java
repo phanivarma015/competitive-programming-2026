4023f21bd01ceb6e5c8c61a2fdc05dec0eedba4e
phanivarma015 <phanindravarmagunturi@gmail.com>
2026-07-07 09:37:11 +0530

    Implement factorial calculation using recursion

---
+++ factorial using recursion.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int p=fact(n);
+        System.out.println(p);
+    }
+    public static int fact(int n)
+    {
+        if(n==1)
+            return 1;
+        else
+            return n*fact(n-1);
+    }
+}

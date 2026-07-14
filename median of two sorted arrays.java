1fd3480199439f5fce9d4dfbc0b984dfe12466c1
phanivarma015 <phanindravarmagunturi@gmail.com>
2026-07-14 23:18:36 +0530

    Add solution for median of two sorted arrays

---
+++ median of two sorted arrays.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         Scanner sc = new Scanner (System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int a[]=new int[m];
+        for(int i=0;i<m;i++)
+            a[i]=sc.nextInt();
+            
+        int b[] = new int[n];
+        for(int i=0;i<n;i++)
+            b[i]=sc.nextInt();
+            
+        int c[]=new int[m+n];
+        int i=0;
+        int j=0;  
+        int k=0; 
+        while(i<m && j<n)
+        {
+                if(a[i]<=b[j])
+                {
+                    c[k++]=a[i++];
+                }
+                else
+                {
+                    c[k++]=b[j++];
+                }
+        }
+        while(i<m)
+        {
+            c[k++]=a[i++];
+        }
+        while(j<n)
+        {
+            c[k++]=b[j++];
+        }
+        double mid=0;
+        if(c.length%2==0)
+        {
+            mid=(c[c.length/2]+c[(c.length/2)-1])/2.0;
+        }
+        else
+            mid=c[c.length/2];
+        
+        System.out.println(mid);
+    }
+}
